f203d9ee1502652c6ed1e2eaae99db7e6e3a420e
Osama AVvan <gamingfreaks786@gmail.com>
2018-01-29 11:11:28 +0500

    fire

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express"), mongoose = require("mongoose");
+
+let app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://bookworm123:assassin123@ds117148.mlab.com:17148/bookworm", (err) => {
+    if (err) return console.log(err);
+
+    console.log("Connection to DB created...")
+});
+
+let db = mongoose.connection;
+
+
+
+app.set("port", process.env.PORT || 8080);
+app.set("env", process.env.NODE_ENV || "development");
+app.set("view engine", "pug");
+
+let router = require("./routes");
+
+app.use("/", router);
+
+app.listen(app.get("port"), () => console.log(`App is listeing on ${app.get("port")}`));
